bcd7f73e2f09e9381046c73652942e64a6b150ee
Chris Karbach <9342749+ckone4You@users.noreply.github.com>
2025-01-16 08:24:45 +0100

    initial commit after 2

---
+++ src/main/java/com/example/workflow/controllers/HomeController.java
@@ -0,0 +1,24 @@
+package com.example.workflow.controllers;
+
+import org.camunda.bpm.engine.ProcessEngine;
+import org.camunda.bpm.engine.ProcessEngines;
+import org.camunda.bpm.engine.runtime.ProcessInstantiationBuilder;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping(value="/home", method = RequestMethod.GET)
+    public String home() {
+        return "Oh Johny";
+    }
+    @RequestMapping(value="/execute", method = RequestMethod.GET)
+    public String execute() {
+        ProcessEngine engine = ProcessEngines.getDefaultProcessEngine();
+        ProcessInstantiationBuilder instance = engine.getRuntimeService().createProcessInstanceByKey("Process_0y4kdqm");
+        instance.executeWithVariablesInReturn();
+        return "bpm executed";
+    }
+}
