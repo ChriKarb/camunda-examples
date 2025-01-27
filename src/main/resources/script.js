93151d9e6b2b8c0f0d7c3939ea49c288c3201904
Chris Karbach <9342749+ckone4You@users.noreply.github.com>
2025-01-27 19:17:54 +0100

    variables from execution

--- src/main/resources/script.js
+++ src/main/resources/script.js
@@ -1,6 +1,9 @@
-print("returnValueUserObj.stringify ::" + JSON.stringify(returnValueUserObj));
+print("returnValueUserObj.stringify within script.js ::" + JSON.stringify(returnValueUserObj));
 
-print("userObj3.stringify ::" + JSON.stringify(userObj3));
+print("userObj3.stringify within script.js ::" + JSON.stringify(userObj3));
+
+print("userObjVariableFromExecution.stringify within script.js ::" + JSON.stringify(userObjVariableFromExecution));
+print("userObjVariableFromExecution.stringify from execution within script.js ::" + JSON.stringify(execution.getVariable('userObjVariableFromExecution')));
 
 
 var userObj1 = {
@@ -8,6 +11,8 @@ var userObj1 = {
     name: "JSname"
 
 }
+
+
 
 print("userObj1 ::" + userObj1.name);
 print("userObj1.stringify ::" + JSON.stringify(userObj1));
