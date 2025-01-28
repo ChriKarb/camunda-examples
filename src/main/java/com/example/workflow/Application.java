fa1d6d97b340243530ef42234d5fb327ab0965e7
Chris Karbach <9342749+ckone4You@users.noreply.github.com>
2025-01-28 18:33:12 +0100

    spring expression delegate needs componentscan

--- src/main/java/com/example/workflow/Application.java
+++ src/main/java/com/example/workflow/Application.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan({"com.example.delegates"})
+@ComponentScan({"com.example"})
 public class Application {
 
   public static void main(String... args) {
